09b0b45932f9d187b179185f7af481925eb931c4
desnos <benoit.desnos66@gmail.com>
2022-09-06 14:52:25 +0200

    auth ok, delete + modify ok. likes not done

---
+++ backend/routes/sauces.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
+const router = express.Router();
+const sauceCtrl = require("../controllers/sauce");
+
+router.get("/", auth, sauceCtrl.displayAllSauces);
+router.post("/", auth, multer, sauceCtrl.createSauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
+router.put("/:id", auth, multer, sauceCtrl.modifySauce);
+router.get("/:id", auth, sauceCtrl.displayOneSauce);
+
+module.exports = router;

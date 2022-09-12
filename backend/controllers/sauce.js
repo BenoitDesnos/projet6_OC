4cde1f65af0c872a17915240e29db8f3034f75eb
desnos <benoit.desnos66@gmail.com>
2022-09-12 17:35:03 +0200

    middleware likes OK, manque commentaires

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,11 +1,70 @@
 const Sauce = require("../models/Sauce");
 const fs = require("fs");
+const { log } = require("console");
+
+exports.likeToggleSauce = (req, res, next) => {
+  if (req.body.like == 1) {
+    Sauce.updateOne(
+      // on filtre
+      { _id: req.params.id },
+      { $push: { usersLiked: req.body.userId }, $inc: { likes: req.body.like } }
+    )
+      .then(() => res.status(200).json({ message: "objet liké" }))
+      .catch((error) => res.status(401).json({ error }));
+  }
+  if (req.body.like == -1) {
+    Sauce.updateOne(
+      { _id: req.params.id },
+      {
+        $push: { usersDisliked: req.body.userId },
+        $inc: { dislikes: 1 },
+      }
+    )
+      .then(() => res.status(200).json({ message: "objet liké" }))
+      .catch((error) => res.status(401).json({ error }));
+  }
+  if (req.body.like == 0) {
+    Sauce.findOne({ _id: req.params.id }).then((sauce) => {
+      log(sauce);
+      let usersThatLiked = false;
+      for (i = 0; i < sauce.usersLiked.length; i++) {
+        if (sauce.usersLiked[i] == req.body.userId) {
+          usersThatLiked = true;
+        }
+      }
+      // si l'utilisateur n'a pas liké cela veut dire qu'il a disliké
+      if (usersThatLiked == false) {
+        Sauce.updateOne(
+          // pas besoin de filtre car déjà filtré dans findone()
+          {},
+          {
+            $pull: { usersDisliked: req.body.userId },
+            $inc: { dislikes: -1 },
+          }
+        )
+          .then(() => res.status(200).json({ message: "Objet modifié !" }))
+          .catch((error) => res.status(400).json({ error }));
+      } else {
+        Sauce.updateOne(
+          {},
+          {
+            $pull: { usersLiked: req.body.userId },
+            $inc: { likes: -1 },
+          }
+        )
+          .then(() => res.status(200).json({ message: "Objet modifié !" }))
+          .catch((error) => res.status(400).json({ error }));
+      }
+    });
+  }
+};
+
 exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
-  /* console.log(sauceObject + " avant delete ids"); */
+  // VOIR PK DELETE ID ET USERID
+  console.log(sauceObject);
   delete sauceObject._id;
   delete sauceObject._userId;
-  /*  console.log(sauceObject + " apres delete ids"); */
   const sauce = new Sauce({
     ...sauceObject,
     userId: req.auth.userId,
@@ -13,6 +72,7 @@ exports.createSauce = (req, res, next) => {
       req.file.filename
     }`,
   });
+  console.log(sauce);
 
   sauce
     .save()
@@ -65,27 +125,6 @@ exports.modifySauce = (req, res, next) => {
     });
 };
 exports.deleteSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-    .then((sauce) => {
-      if (sauce.userId != req.auth.userId) {
-        res.status(401).json({ message: "Not authorized" });
-      } else {
-        const filename = sauce.imageUrl.split("/images/")[1];
-        fs.unlink(`images/${filename}`, () => {
-          Sauce.deleteOne({ _id: req.params.id })
-            .then(() => {
-              res.status(200).json({ message: "Objet supprimé !" });
-            })
-            .catch((error) => res.status(401).json({ error }));
-        });
-      }
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
-};
-
-exports.likeToggleSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
       if (sauce.userId != req.auth.userId) {
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -9,5 +9,6 @@ router.post("/", auth, multer, sauceCtrl.createSauce);
 router.delete("/:id", auth, sauceCtrl.deleteSauce);
 router.put("/:id", auth, multer, sauceCtrl.modifySauce);
 router.get("/:id", auth, sauceCtrl.displayOneSauce);
+router.post("/:id/like", auth, sauceCtrl.likeToggleSauce);
 
 module.exports = router;
